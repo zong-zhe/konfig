dffea9497775f99935a694e15971d48ef247e785
zongz <zongzhe1024@163.com>
2024-07-29 17:11:31 +0800

    test

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,13 +8,13 @@
 set -e
 
 # 定义输出文件
-OUTPUT_FILE="/Users/zongz/Workspace/kusionstack/konfig/test_results.txt"
+# OUTPUT_FILE="/Users/zongz/Workspace/kusionstack/konfig/test_results.txt"
 
 # 清空输出文件
 > "$OUTPUT_FILE"
 
 find ./examples -name "kcl.mod" -exec dirname {} \; | while read -r dir; do
-    if (cd "$dir" && kcl run >> "$OUTPUT_FILE" 2>&1); then
+    if (cd "$dir" && kcl run); then
         echo "\033[32mTest SUCCESSED - $dir\033[0m\n" | tee -a "$OUTPUT_FILE"
     else
         echo "\033[31mTest FAILED - $dir\033[0m\n" | tee -a "$OUTPUT_FILE"
